f5b5340ea2897e62bc2fbbac90c845e074863a9b
Kittisuk Suksamai <52103598+lovetonglpfcs@users.noreply.github.com>
2020-03-11 23:13:04 +0700

    Add files via upload

--- ProjectMKII/src/MKII.java
+++ ProjectMKII/src/MKII.java
@@ -1,30 +1,21 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.*;
 import net.proteanit.sql.DbUtils;
-
-import javax.swing.JApplet;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.ListSelectionModel;
-import javax.swing.border.LineBorder;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.JComboBox;
+import javax.swing.*;
 
 
 public class MKII extends JApplet implements ActionListener{
 	public MKII() {
 		getContentPane().setBackground(Color.DARK_GRAY);
 	}
+	boolean trig = false;
+	Timer afk = new Timer(30000,this);
+	private int Smoney=0;
+	private String passLogin;
 	private Connection con =null;
 	private ResultSet rs = null;
 	private PreparedStatement pst =null;
@@ -33,6 +24,23 @@ public class MKII extends JApplet implements ActionListener{
 	private JScrollPane scrollPane_1,scrollPane_2;
 	private JTable tbcustomers,thistable;
 	private JTable tbhistory;
+	private JTextField carregis;
+	private JTextField carbrand;
+	private JTextField carmodel;
+	private JTextField carprice;
+	private JTextField cartype;
+	JButton btnNewButton_2,Login,Admin,Addcar,btnNewButton_1;
+	private JTextField customname;
+	private JTextField addresscustom;
+	private JComboBox combodriverlc;
+	private JPanel panel_2;
+	private JLabel lblNewLabel_9;
+	private JLabel lblNewLabel_12;
+	private JLabel lblNewLabel_13;
+	private JButton btnNewButton_3;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnRent;
 	public void init() {
 		
 		con=connect.ConnectDB();
@@ -40,15 +48,14 @@ public class MKII extends JApplet implements ActionListener{
 		getContentPane().setLayout(null);
 		
 		tbcars = new JTable();
+		tbcars.setLocation(0, 77);
 		tbcars.setSurrendersFocusOnKeystroke(true);
 		tbcars.setFillsViewportHeight(true);
 		tbcars.setCellSelectionEnabled(true);
 		tbcars.setColumnSelectionAllowed(true);
-		tbcars.setBounds(235, 29, 527, 188);
 		tbcars.setPreferredScrollableViewportSize(new Dimension(450,63));
 		tbcars.setFillsViewportHeight(true);
 		
-		getContentPane().add(tbcars);
 		
 		JScrollPane scrollPane = new JScrollPane(tbcars);
 		scrollPane.setBounds(399, 98, 789, 202);
@@ -67,56 +74,208 @@ public class MKII extends JApplet implements ActionListener{
 		scrollPane_2 = new JScrollPane(tbhistory);
 		scrollPane_2.setBounds(399, 400, 789, 239);
 		getContentPane().add(scrollPane_1);
+		
+		JLabel lblNewLabel = new JLabel("418 Garage");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.CYAN);
+		lblNewLabel.setFont(new Font("Tw Cen MT Condensed", Font.PLAIN, 39));
+		lblNewLabel.setBounds(0, 13, 1200, 72);
+		getContentPane().add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.PINK);
+		panel.setBounds(31, 93, 310, 280);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("New Car");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblNewLabel_1.setForeground(SystemColor.textHighlight);
+		lblNewLabel_1.setBounds(12, 13, 97, 34);
+		panel.add(lblNewLabel_1);
+		
+		carregis = new JTextField();
+		carregis.setBounds(81, 57, 185, 22);
+		panel.add(carregis);
+		carregis.setColumns(10);
+		
+		carbrand = new JTextField();
+		carbrand.setColumns(10);
+		carbrand.setBounds(81, 92, 185, 22);
+		panel.add(carbrand);
+		
+		carmodel = new JTextField();
+		carmodel.setColumns(10);
+		carmodel.setBounds(81, 127, 185, 22);
+		panel.add(carmodel);
+		
+		carprice = new JTextField();
+		carprice.setColumns(10);
+		carprice.setBounds(81, 162, 185, 22);
+		panel.add(carprice);
+		
+		cartype = new JTextField();
+		cartype.setColumns(10);
+		cartype.setBounds(81, 197, 185, 22);
+		panel.add(cartype);
+		
+		JLabel lblNewLabel_2 = new JLabel("\u0E1B\u0E49\u0E32\u0E19\u0E17\u0E30\u0E40\u0E1A\u0E35\u0E22\u0E19");
+		lblNewLabel_2.setBounds(12, 63, 94, 16);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("\u0E22\u0E35\u0E48\u0E2B\u0E49\u0E2D");
+		lblNewLabel_3.setBounds(12, 98, 56, 16);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("\u0E23\u0E38\u0E48\u0E19");
+		lblNewLabel_4.setBounds(12, 133, 56, 16);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("\u0E23\u0E32\u0E04\u0E32/\u0E27\u0E31\u0E19");
+		lblNewLabel_5.setBounds(12, 168, 56, 16);
+		panel.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("\u0E1B\u0E23\u0E30\u0E40\u0E20\u0E17");
+		lblNewLabel_6.setBounds(12, 203, 56, 16);
+		panel.add(lblNewLabel_6);
+		
+		Addcar = new JButton("Add");
+		Addcar.setBounds(12, 242, 97, 25);
+		panel.add(Addcar);
+		
+		btnNewButton_1 = new JButton("Edit");
+		btnNewButton_1.setBounds(121, 242, 71, 25);
+		panel.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("Delete");
+		btnNewButton_2.setBounds(204, 242, 97, 25);
+		panel.add(btnNewButton_2);
+		
+		Admin = new JButton("Admin");
+		Admin.setBounds(1091, 668, 97, 25);
+		getContentPane().add(Admin);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.ORANGE);
+		panel_1.setBounds(31, 400, 310, 239);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_7 = new JLabel("New Customers");
+		lblNewLabel_7.setForeground(Color.BLUE);
+		lblNewLabel_7.setFont(new Font("Sitka Subheading", Font.PLAIN, 16));
+		lblNewLabel_7.setBounds(12, 13, 188, 16);
+		panel_1.add(lblNewLabel_7);
+		
+		customname = new JTextField();
+		customname.setBounds(103, 56, 163, 22);
+		panel_1.add(customname);
+		customname.setColumns(10);
+		
+		JLabel lblNewLabel_8 = new JLabel("Name");
+		lblNewLabel_8.setBounds(12, 59, 56, 16);
+		panel_1.add(lblNewLabel_8);
+		
+		addresscustom = new JTextField();
+		addresscustom.setBounds(103, 91, 163, 22);
+		panel_1.add(addresscustom);
+		addresscustom.setColumns(10);
+		
+		JLabel lblNewLabel_10 = new JLabel("Address");
+		lblNewLabel_10.setBounds(12, 94, 56, 16);
+		panel_1.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("Driver's License");
+		lblNewLabel_11.setBounds(12, 129, 97, 16);
+		panel_1.add(lblNewLabel_11);
+		
+		JButton addcustom = new JButton("Add");
+		addcustom.setBounds(12, 201, 79, 25);
+		panel_1.add(addcustom);
+		
+		JButton deletecustom = new JButton("Delete");
+		deletecustom.setBounds(219, 201, 79, 25);
+		panel_1.add(deletecustom);
+		
+		JButton editcustom = new JButton("Edit");
+		editcustom.setBounds(103, 201, 97, 25);
+		panel_1.add(editcustom);
+		
+		combodriverlc = new JComboBox();
+		combodriverlc.setModel(new DefaultComboBoxModel(new String[] {"Have", "No"}));
+		combodriverlc.setBounds(169, 126, 97, 22);
+		panel_1.add(combodriverlc);
+		
+		Login = new JButton("Login");
+		Login.setBounds(969, 668, 97, 25);
+		getContentPane().add(Login);
 		showDT();
+		btnNewButton_2.addActionListener(this);
 		combocars.addActionListener(this);
+		Login.addActionListener(this);
+		
+		carregis.setEditable(false);
+		carmodel.setEditable(false);
+		carbrand.setEditable(false);
+		carprice.setEditable(false);
+		cartype.setEditable(false);
+		Admin.setEnabled(false);
+		Addcar.setEnabled(false);
+		btnNewButton_2.setEnabled(false);
+		btnNewButton_1.setEnabled(false);
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(new Color(102, 255, 204));
+		panel_2.setBounds(31, 668, 894, 89);
+		getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		lblNewLabel_9 = new JLabel("Staff");
+		lblNewLabel_9.setFont(new Font("Tw Cen MT Condensed", Font.PLAIN, 26));
+		lblNewLabel_9.setForeground(new Color(0, 0, 255));
+		lblNewLabel_9.setBounds(0, 0, 45, 16);
+		panel_2.add(lblNewLabel_9);
+		
+		lblNewLabel_12 = new JLabel("Car ");
+		lblNewLabel_12.setBounds(35, 29, 56, 16);
+		panel_2.add(lblNewLabel_12);
+		
+		lblNewLabel_13 = new JLabel("Customer ID");
+		lblNewLabel_13.setBounds(35, 60, 116, 16);
+		panel_2.add(lblNewLabel_13);
+		
+		btnNewButton_3 = new JButton("Return the car");
+		btnNewButton_3.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+		btnNewButton_3.setForeground(new Color(0, 153, 255));
+		btnNewButton_3.setBounds(633, 17, 236, 59);
+		panel_2.add(btnNewButton_3);
+		
+		textField = new JTextField();
+		textField.setBounds(155, 57, 187, 22);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(155, 26, 187, 22);
+		panel_2.add(textField_1);
+		textField_1.setColumns(10);
+		
+		btnRent = new JButton("Rent");
+		btnRent.setForeground(new Color(255, 51, 51));
+		btnRent.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+		btnRent.setBounds(385, 17, 236, 59);
+		panel_2.add(btnRent);
+		
+		
 	}
 	public void showDT() {
 		String sql = "select * from cars";
 		try {
 			pst = con.prepareStatement(sql);
 			rs = pst.executeQuery();
-			while(rs.next()) {
-				System.out.println(rs.getString("Price/Day"));
-			}
-			tbcars.setModel(new DefaultTableModel(
-				new Object[][] {
-					{"\u0E01\u0E01-4640", "Hyundai", "Elite", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E01\u0E04-9452", "Nissan", "March", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E01\u0E08-4497", "Honda", "Jazz", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E01\u0E08-5869", "Nissan", "Sylphy", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E01\u0E09-6348", "Isuzu", "D-Max Hi Render", null, "\u0E23\u0E16\u0E01\u0E23\u0E30\u0E1A\u0E30", "complete"},
-					{"\u0E01\u0E10-9213", "Toyota", "Revo", null, "\u0E23\u0E16\u0E01\u0E23\u0E30\u0E1A\u0E30", "complete"},
-					{"\u0E01\u0E13-7149", "Toyota", "Innova", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E01\u0E15-5487", "Toyota", "Yaris", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E01\u0E17-2378", "Honda", "Amaze", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E01\u0E19-6150", "Nissan", "Almera", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E01\u0E21-2359", "Hyundai", "H-1", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E01\u0E23-1219", "Nissan", "Teana", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E01\u0E2A-7021", "Honda", "Civic", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E01\u0E2D-2546", "BMW", "X1", null, "\u0E23\u0E16\u0E2A\u0E1B\u0E2D\u0E23\u0E4C\u0E15", "complete"},
-					{"\u0E02\u0E15-2118", "Honda", "Spada", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E0A\u0E22-3711", "Hyundai", "Deluxe", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E0A\u0E29-6688", "BMW", "230d", null, "\u0E23\u0E16\u0E2A\u0E1B\u0E2D\u0E23\u0E4C\u0E15", "complete"},
-					{"\u0E0D\u0E15-6011", "Mercedes Benz", "C220d", null, "\u0E23\u0E16\u0E2A\u0E1B\u0E2D\u0E23\u0E4C\u0E15", "complete"},
-					{"\u0E0E\u0E01-1773", "Mercedes Benz", "C350", null, "\u0E23\u0E16\u0E2A\u0E1B\u0E2D\u0E23\u0E4C\u0E15", "complete"},
-					{"\u0E16\u0E0A-8449", "Isuzu", "D-Max Space cab", null, "\u0E23\u0E16\u0E01\u0E23\u0E30\u0E1A\u0E30", "complete"},
-					{"\u0E1A\u0E18-1989", "Honda", "Brio", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E1A\u0E1E-4933", "Mitsubishi", "Mirage", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E1A\u0E23-8796", "Honda", "Mobilio", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E1A\u0E29-8452", "Honda", "City", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E1C\u0E01-2394", "Mitsubishi", "Attrage", null, "\u0E23\u0E16\u0E02\u0E19\u0E32\u0E14\u0E40\u0E25\u0E47\u0E01", "complete"},
-					{"\u0E1C\u0E04-1638", "Toyota", "Altis", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-					{"\u0E1C\u0E08-6754", "Toyota", "Sienta", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E1C\u0E19-3922", "Isuzu", "HR", null, "\u0E23\u0E16\u0E01\u0E23\u0E30\u0E1A\u0E30", "complete"},
-					{"\u0E1C\u0E2B-3215", "Mitsubishi", "Xpander", null, "\u0E23\u0E16\u0E15\u0E39\u0E49", "complete"},
-					{"\u0E29\u0E2B-4681", "Toyota", "Vios", null, "\u0E23\u0E16\u0E40\u0E01\u0E4B\u0E07", "complete"},
-				},
-				new String[] {
-					"Vehicle registration", "Car brand", "Car model", "Price/Day", "Type car", "status"
-				}
-			));
+			tbcars.setModel(DbUtils.resultSetToTableModel(rs));
 		} catch (SQLException e) {
-			//Logger.getLogger(MKII.class.getName()).log(Level.SEVERE,null,e);
+			Logger.getLogger(MKII.class.getName()).log(Level.SEVERE,null,e);
 			e.printStackTrace();
 		}
 		sql = "select * from customers";
@@ -124,7 +283,8 @@ public class MKII extends JApplet implements ActionListener{
 			pst = con.prepareStatement(sql);
 			rs = pst.executeQuery();
 			tbcustomers.setModel(DbUtils.resultSetToTableModel(rs));
-		}catch (Exception e) {
+		}catch (SQLException e) {
+			Logger.getLogger(MKII.class.getName()).log(Level.SEVERE,null,e);
 			// TODO: handle exception
 		}
 		sql = "select * from history";
@@ -132,17 +292,57 @@ public class MKII extends JApplet implements ActionListener{
 			pst = con.prepareStatement(sql);
 			rs = pst.executeQuery();
 			tbhistory.setModel(DbUtils.resultSetToTableModel(rs));
-		}catch (Exception e) {
+		}catch (SQLException e) {
+			Logger.getLogger(MKII.class.getName()).log(Level.SEVERE,null,e);
 			// TODO: handle exception
 		}
 	}
 
-	public void actionPerformed(ActionEvent event) {
-		System.out.println(combocars.getSelectedItem()=="History");
+	public void actionPerformed(ActionEvent e) {
+		if(trig) {
+			trig=false;
+			afk.stop();
+			carregis.setEditable(false);
+			carmodel.setEditable(false);
+			carbrand.setEditable(false);
+			carprice.setEditable(false);
+			cartype.setEditable(false);
+			Admin.setEnabled(false);
+			Addcar.setEnabled(false);
+			btnNewButton_2.setEnabled(false);
+			btnNewButton_1.setEnabled(false);
+		}
 		if(combocars.getSelectedItem()=="Customers") {
 			getContentPane().add(scrollPane_1);
+			getContentPane().remove(scrollPane_2);
 		}else if(combocars.getSelectedItem()=="History"){
 			getContentPane().add(scrollPane_2);
+			getContentPane().remove(scrollPane_1);
+		}
+		if(btnNewButton_2==e.getSource()) {
+			if (JOptionPane.showConfirmDialog(null, "[Delete].....Are you sure?", "WARNING",	JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			    // yes option
+			} else {
+			    // no option
+			}
+		}else if(e.getSource()==Login) {
+			passLogin=JOptionPane.showInputDialog("Password?");
+			if(Integer.parseInt(passLogin)==1234) {
+				carregis.setEditable(true);
+				carmodel.setEditable(true);
+				carbrand.setEditable(true);
+				carprice.setEditable(true);
+				cartype.setEditable(true);
+				Admin.setEnabled(true);
+				Addcar.setEnabled(true);
+				btnNewButton_2.setEnabled(true);
+				btnNewButton_1.setEnabled(true);
+				trig=true;
+				afk.start();
+			}else {
+				JOptionPane.showMessageDialog(null,"Wrong password!!!");
+			}
+			
 		}
 	}
 }
